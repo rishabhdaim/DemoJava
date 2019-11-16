741f8de64bd079b08048baa89dbe3c91446dce34
Rishabh Kumar <diam@adobe.com>
2019-11-16 19:29:23 +0530

    Preview Features for Java 13

---
+++ src/java13/BenchMark.java
@@ -0,0 +1,30 @@
+package java13;
+
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  * time java --enable-preview --source 13 -Xms4G -Xmx8G -XX:StartFlightRecording=disk=true,dumponexit=true,filename=G1.jfr,maxsize=1024m,settings=profile BenchMark
+ *  * time java --enable-preview --source 13 -XX:+UseParallelOldGC -Xms4G -Xmx8G -XX:StartFlightRecording=disk=true,dumponexit=true,filename=ParallelOld.jfr,maxsize=1024m,settings=profile BenchMark
+ *  * time java --enable-preview --source 13 -XX:+UseConcMarkSweepGC -Xms4G -Xmx8G -XX:StartFlightRecording=disk=true,dumponexit=true,filename=CMS.jfr,maxsize=1024m,settings=profile BenchMark
+ *  * time java --enable-preview --source 13 -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xms4G -Xmx8G -XX:StartFlightRecording=disk=true,dumponexit=true,filename=Shenandoah.jfr,maxsize=1024m,settings=profile BenchMark
+ *  * time java --enable-preview --source 13 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xms4G -Xmx8G -XX:StartFlightRecording=disk=true,dumponexit=true,filename=ZGC.jfr,maxsize=1024m,settings=profile BenchMark
+ *
+ *  * time java -XX:ArchiveClassesAtExit=temp.jsa BenchMark.java
+ *  * time java -XX:SharedArchiveFile=temp.jsa BenchMark.java
+ *
+ */
+
+public class BenchMark {
+    static List<Object> l;
+
+    public static void main(String... args) {
+        l = new ArrayList<>();
+        for (int c = 0; c < 200_000_000; c++) {
+            l.add(new Object());
+        }
+        System.out.println("Hello Java 13!");
+        System.out.println("Running " + ManagementFactory.getRuntimeMXBean().getUptime() + "ms.");
+    }
+}
